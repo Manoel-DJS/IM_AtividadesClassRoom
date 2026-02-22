c29391e1f0cab53efffe611e286d19c13e7fbc04
Manoel <vinesouza9x8@gmail.com>
2026-02-22 12:23:35 -0300

    exemplo de teste

--- src/main/java/org/atividade/Main.java
+++ src/main/java/org/atividade/Main.java
@@ -1,17 +1,49 @@
 package org.atividade;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.atividade.entities.*;
+import org.atividade.exceptions.RegraNegocioException;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SistemaCarbono sistema = new SistemaCarbono();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Proprietários (PF e PJ)
+        Proprietario pf = new PessoaFisica("João da Silva", "123.456.789-00");
+        Proprietario pj = new PessoaJuridica("Empresa Verde S.A.", "12.345.678/0001-99");
+        Proprietario pf2 = new PessoaFisica("Maria Oliveira", "987.654.321-00");
+        Proprietario pf3 = new PessoaFisica("Carlos Souza", "111.222.333-44");
+
+        sistema.cadastrarProprietario(pf);
+        sistema.cadastrarProprietario(pj);
+        sistema.cadastrarProprietario(pf2);
+        sistema.cadastrarProprietario(pf3);
+
+        // Lote (sempre 1000 créditos)
+        LoteCreditoCarbono lote = sistema.criarLote("LOTE-OLIMPIADAS-0001");
+
+        // Árvores (rastreabilidade com geolocalização)
+        sistema.registrarArvore(lote.getId(), new ArvoreGeradoraCredito("Ipê Amarelo", -10.916200, -37.668300));
+        sistema.registrarArvore(lote.getId(), new ArvoreGeradoraCredito("Aroeira", -10.916210, -37.668310));
+        sistema.registrarArvore(lote.getId(), new ArvoreGeradoraCredito("Pau Brasil", -10.916220, -37.668320));
+
+        // Primeiro proprietário (produção/entrada no sistema)
+        sistema.definirPrimeiroProprietario(lote.getId(), pf);
+
+        // Vendas (histórico). Máximo 3 proprietários no histórico.
+        sistema.venderLote(lote.getId(), pf, pj, new BigDecimal("1500.00"));
+        sistema.venderLote(lote.getId(), pj, pf2, new BigDecimal("1700.50"));
+
+        // Tentativa de 3ª venda (viraria 4º dono) -> deve falhar
+        try {
+            sistema.venderLote(lote.getId(), pf2, pf3, new BigDecimal("1800.00"));
+        } catch (RegraNegocioException ex) {
+            System.out.println("\n[ERRO ESPERADO] " + ex.getMessage());
         }
+
+        // Relatório no console
+        System.out.println("\n==================== RELATÓRIO DO LOTE ====================");
+        sistema.imprimirRelatorioLote(lote.getId());
     }
 }
